69f4c97e31e4559c26858cf89bd7d9b981664427
Stefan Gojan <vollekannehoschi@gmail.com>
2018-02-19 19:09:53 +0100

    keymap change test

--- make.sh
+++ make.sh
@@ -5,4 +5,4 @@ make ARDUINO_BUILDER_PATH="/usr/bin/arduino-builder" \
      ARDUINO_LOCAL_LIB_PATH="../arduino-local" \
      AVR_GCC_PREFIX="/usr" \
      ARDUINO_TOOLS_PATH="/usr/share/arduino/tools" \
-     AVR_SIZE_PATH="avr-size"
+     AVR_SIZE_PATH="avr-size" flash
